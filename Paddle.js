a7879b4fbc486da605b96d248b588a6167d6224b
Guilherme Saliba <salibagui19@gmail.com>
2023-09-16 21:39:23 -0300

    initial commit

---
+++ Paddle.js
@@ -0,0 +1,30 @@
+const SPEED = 0.0175;
+
+export default class Paddle {
+  constructor(paddleElem) {
+    this.paddleElem = paddleElem;
+    this.reset();
+  }
+
+  get position() {
+    return parseFloat(
+      getComputedStyle(this.paddleElem).getPropertyValue('--position')
+    );
+  }
+
+  set position(value) {
+    this.paddleElem.style.setProperty('--position', value);
+  }
+
+  reset() {
+    this.position = 50;
+  }
+
+  rect() {
+    return this.paddleElem.getBoundingClientRect();
+  }
+
+  update(delta, ballHeight) {
+    this.position += SPEED * delta * (ballHeight - this.position);
+  }
+}
