71066c78bec07a057d30061c9e9ac32778f4006c
Guilherme Saliba <salibagui19@gmail.com>
2025-01-15 02:30:10 -0300

    chore: add transpilePackages to `next.config.ts`

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
-  /* config options here */  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  /* config options here */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  transpilePackages: ["next-mdx-remote"],
 };
 
-const withMDX = createMDX({
-
-});
+const withMDX = createMDX({});
 
 export default withMDX(nextConfig);
